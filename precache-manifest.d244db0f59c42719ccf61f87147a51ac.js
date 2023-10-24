5d44c47ed613818174e13d43d0bb572c3db30a99
Code Sandbox <hello@codesandbox.io>
2023-10-24 12:50:28 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.d244db0f59c42719ccf61f87147a51ac.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29c7353e0871a3532508664c13d26496",
+    "url": "https://naveendoddi.github.io/csb-rygl83/index.html"
+  },
+  {
+    "revision": "feb9773f7bcbaf0dd206",
+    "url": "https://naveendoddi.github.io/csb-rygl83/static/css/main.8931a3d2.chunk.css"
+  },
+  {
+    "revision": "bd78319e2d1f254e3c91",
+    "url": "https://naveendoddi.github.io/csb-rygl83/static/js/2.83b3512d.chunk.js"
+  },
+  {
+    "revision": "16ed9d8ca05bfaa905a3711eb865eada",
+    "url": "https://naveendoddi.github.io/csb-rygl83/static/js/2.83b3512d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "227c636594b5746aba82",
+    "url": "https://naveendoddi.github.io/csb-rygl83/static/js/3.47fa84d7.chunk.js"
+  },
+  {
+    "revision": "feb9773f7bcbaf0dd206",
+    "url": "https://naveendoddi.github.io/csb-rygl83/static/js/main.61a7202a.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "https://naveendoddi.github.io/csb-rygl83/static/js/main.61a7202a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc347610f6272ec89815",
+    "url": "https://naveendoddi.github.io/csb-rygl83/static/js/runtime-main.95e61ae7.js"
+  }
+]);
